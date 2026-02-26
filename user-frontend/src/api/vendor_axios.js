8683cd12723dd0ba5bdf64c776ef7c18b95073c7
Mohana1022 <pmohana1022@gmail.com>
2026-02-26 16:06:04 +0530

    render added

--- user-frontend/src/api/vendor_axios.js
+++ user-frontend/src/api/vendor_axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL =
-    import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+    import.meta.env.VITE_API_BASE_URL || "https://final-shopsphere-8v1c.onrender.com";
 
 // ─── JWT AUTO-REFRESH INTERCEPTOR ───────────────────────────────────────────
 let isRefreshing = false;
